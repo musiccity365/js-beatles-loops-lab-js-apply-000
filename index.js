f01b0f7b7187b521e65519dbe5f35b6f87199b5b
Michael Sackstein <msackste@masonlive.gmu.edu>
2020-01-13 19:55:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for (var i = 0; i < musicians.length; i++){
-    array.push(musicians[i] + ' plays ' + )
+    array.push(musicians[i] + ' plays ' + instruments[i]);
   }
 }
